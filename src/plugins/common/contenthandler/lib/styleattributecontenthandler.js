78dc74739d5cbecb0ccab4d5d848c3ac3f5b90f4
Bob Czarnecki <rczarnecki@constantcontact.com>
2013-02-20 15:17:16 -0500

    VN-1473 Add FF fix for removing comments that show up as text nodes

--- src/plugins/common/contenthandler/lib/styleattributecontenthandler.js
+++ src/plugins/common/contenthandler/lib/styleattributecontenthandler.js
@@ -1,9 +1,75 @@
 define([
-    'aloha/core',
-    'jquery',
-    'aloha/contenthandlermanager'
-], function(Aloha, jQuery, ContentHandlerManager) {
-    var config = null,
+    define([
+        'jquery',
+        'aloha',
+        'aloha/contenthandlermanager'
+    ], function($, Aloha, ContentHandlerManager) {
+        var config = null,
+            defaults = {
+                supportedStyles: {
+                    'text-align': true
+                }
+            };
+
+        Aloha.settings.contentHandler = Aloha.settings.contentHandler || {};
+        Aloha.settings.contentHandler.handler = Aloha.settings.contentHandler.handler || {};
+        Aloha.settings.contentHandler.handler.styleattribute =  Aloha.settings.contentHandler.handler.styleattribute || {};
+
+
+        var init = function() {
+            if (!config) {
+                config = $.extend(true, {}, defaults, Aloha.settings.contentHandler.handler.styleattribute.allowable);
+            }
+        };
+
+        var cleanStyles = function(cfg, $el) {
+            var style, styleItem, styleItemSplit, styleItems, styles, _i, _len;
+
+            style = $el.attr('style');
+
+            if (style) {
+                styleItems = '';
+                styles = style.split(';');
+
+                for (_i = 0, _len = styles.length; _i < _len; _i++) {
+                    styleItem = styles[_i];
+                    styleItem = styleItem.trim();
+                    styleItemSplit = styleItem.split(':');
+
+                    if (styleItemSplit.length !== 2) continue;
+
+                    if (cfg.supportedStyles[styleItemSplit[0].trim()]) {
+                        styleItems += "" + styleItem + ";";
+                    }
+                }
+
+                $el.removeAttr('style');
+                if (styleItems) {
+                    $el.attr('style', "" + styleItems);
+                }
+            }
+        };
+
+        var cleanStyleAttribute = function($content) {
+            $content.children().each(function() {
+                var $child = $(this);
+                cleanStyleAttribute($child);
+                cleanStyles(config, $child);
+            });
+        };
+
+        var StyleAttributeContentHandler = ContentHandlerManager.createHandler({
+            handleContent: function(content)  {
+                init();
+
+                $content = $('<div/>').append(content);
+                cleanStyleAttribute($content);
+                return $content.html();
+            }
+        });
+
+        return StyleAttributeContentHandler;
+    });    var config = null,
         defaults = {
             supportedStyles: {
                 'text-align': true
@@ -17,7 +83,7 @@ define([
 
     var init = function() {
         if (!config) {
-            config = jQuery.extend(true, {}, defaults, Aloha.settings.contentHandler.handler.styleattribute.allowable);
+            config = $.extend(true, {}, defaults, Aloha.settings.contentHandler.handler.styleattribute.allowable);
         }
     };
 
@@ -51,7 +117,7 @@ define([
 
     var cleanStyleAttribute = function($content) {
         $content.children().each(function() {
-            var $child = jQuery(this);
+            var $child = $(this);
             cleanStyleAttribute($child);
             cleanStyles(config, $child);
         });
@@ -61,7 +127,7 @@ define([
         handleContent: function(content)  {
             init();
 
-            $content = jQuery('<div/>').append(content);
+            $content = $('<div/>').append(content);
             cleanStyleAttribute($content);
             return $content.html();
         }
