7f1993a1438c25440ad962806df2d6eb8a076936
Bob Czarnecki <rczarnecki@constantcontact.com>
2014-01-30 15:38:11 -0500

    upgrade to v24.12

--- src/lib/aloha/core.js
+++ src/lib/aloha/core.js
@@ -296,7 +296,7 @@ define([
 		 * It should be set by us and updated for the particular branch
 		 * @property
 		 */
-		version: 'v22.7-g',
+		version: 'v24.12-a',
 
 		/**
 		 * Array of editables that are managed by Aloha
